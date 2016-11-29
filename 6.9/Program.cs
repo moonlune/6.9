4063845c7ab8866bda5deaded18d9a09d2364704
Chen <Chen@DESKTOP-H0ASR75>
2016-11-29 10:25:09 +0800

    添加项目文件。

---
+++ 6.9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._9
+{
+    class x
+    {
+        public const double PI = 3.141592653589;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pi={0}",x.PI);
+            Console.ReadLine();
+        }
+    }
+}
